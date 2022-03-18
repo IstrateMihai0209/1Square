9b6768c1158c91b4a04541668b788dc7868ccfb7
Respekt17 <63280465+Respekt17@users.noreply.github.com>
2022-03-18 08:21:41 +0200

    Added a menu and levels

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,7 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void SelectLevel(int scene)
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            ReloadScene();
+    }
+
+    public void SelectScene(int scene)
     {
         SceneManager.LoadScene(scene);
     }
@@ -11,6 +17,11 @@ public class MainMenu : MonoBehaviour
     public void NextScene()
     { 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void QuitGame()
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float jumpForce = 7f;
-    public float movSpeed = 1.41f;
+    private float jumpForce = 3f;
+    private float movSpeed = 100f;
+
+    private float coyoteTime = 1f;
+    private float timeLeftGrounded;
+
+    private float jumpBuffer = 0.3f;
+    private float lastJumpPressed;
 
     private Rigidbody2D rb;
 
@@ -17,30 +23,29 @@ public class PlayerMovement : MonoBehaviour
         bool left = Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
         bool right = Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
 
-        float dir = Input.GetAxis("Horizontal");
-
-        if (left && Mathf.Abs(rb.velocity.y) < 0.001f)
+        //Try this, but with two last jump pressed variables (for left and right)
+        if ((left || lastJumpPressed + jumpBuffer > Time.time) && Mathf.Abs(rb.velocity.y) < 0.001f && timeLeftGrounded + coyoteTime > Time.time)
         {
+            transform.position += new Vector3(-1, 0, 0) * Time.fixedDeltaTime * movSpeed;
             rb.AddForce(new Vector2(0.00f, jumpForce), ForceMode2D.Impulse);
         }
 
-        if (right && Mathf.Abs(rb.velocity.y) < 0.001f)
+        if ((right || lastJumpPressed + jumpBuffer > Time.time) && Mathf.Abs(rb.velocity.y) < 0.001f && timeLeftGrounded + coyoteTime > Time.time)
         {
+            transform.position += new Vector3(1, 0, 0) * Time.fixedDeltaTime * movSpeed;
             rb.AddForce(new Vector2(0.00f, jumpForce), ForceMode2D.Impulse);
         }
 
-        if(rb.velocity.y < 0f)
-        {
-            transform.position =
-                Vector2.Lerp(transform.position, new Vector2(transform.position.x + 2f * dir, transform.position.y)
-                , movSpeed * Time.deltaTime);
+        if ((left || right) && !(Mathf.Abs(rb.velocity.y) < 0.001f))
+            lastJumpPressed = Time.time;
 
+        if (rb.velocity.y == 0f)
+        {
+            timeLeftGrounded = Time.time;
         }
 
-        if (rb.velocity.y == 0)
-            transform.position = new Vector2(Mathf.Round(transform.position.x), transform.position.y);
-
-        //print(rb.velocity.y);
+        print(lastJumpPressed + jumpBuffer);
+        print("Time.time = " + Time.time);
     }
 }
 
